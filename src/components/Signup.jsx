87bad272ab6cb37cb322ee56a192934c0666c282
Varshini-ka <varshini.2305150@srec.ac.in>
2025-04-22 19:25:22 +0530

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -30,7 +30,7 @@ function Signup() {
     }
 
     try {
-      const response = await axios.post("http://localhost:5000/api/signup", {
+      const response = await axios.post("https://gestura-backend.onrender.com/api/signup", {
         name,
         email,
         password,
